6c0547ba27793af7ca579af4912989aae25f7659
Naman Goyal <raghavgoyal151101@gmail.com>
2024-06-05 02:05:24 -0400

    Added Registered User Vehicles Table

--- src/pages/vehicles/vehicles.jsx
+++ src/pages/vehicles/vehicles.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
 import Page from '../../layouts/Page/Page'
 
-function Vehicles() {
+import React, { useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import './vehicles.css';
+
+const Vehicle = () => {
+  const [vehicles, setVehicles] = useState([
+    { username: 'John Doe', vehicleNumber: 'ABC123', id: 1 },
+    { username: 'Jane Smith', vehicleNumber: 'XYZ456', id: 2 },
+  ]);
+
   return (
     <Page>
-     <h1>Vehicles</h1>
-   </Page>
-  )
-}
+      <h1>Registed Vehicles</h1>
+    <TableContainer>
+      <Table className="vehicle-table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Username</TableCell>
+            <TableCell>Vehicle Number</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {vehicles.map((vehicle) => (
+            <TableRow key={vehicle.id}>
+              <TableCell>{vehicle.username}</TableCell>
+              <TableCell>{vehicle.vehicleNumber}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Page>
+  );
+};
 
-export default Vehicles
+export default Vehicle;
