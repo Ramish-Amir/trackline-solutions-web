7e63612d08ce3d2f755f8960395b62c60cd945a7
Ramish Amir <ramishamir2750@gmail.com>
2024-07-08 23:51:45 -0400

    Completed trip details page header

--- src/pages/tripDetails/tripDetails.jsx
+++ src/pages/tripDetails/tripDetails.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Page from "../../layouts/Page/Page";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { colors } from "../../assets";
 
 function TripDetails() {
-  return <div>TripDetails</div>;
+  const navigate = useNavigate();
+  return (
+    <Page>
+      <div className="pageHeader">
+        <h1>Trip Details</h1>
+        <div className="headerActions">
+          <Button
+            sx={{
+              bgcolor: colors.primary,
+              borderRadius: "10px",
+              "&:hover": {
+                opacity: 0.9,
+                bgcolor: colors.primary,
+              },
+            }}
+            variant="contained"
+            // startIcon={<AddIcon />}
+            onClick={() => navigate("/trips")}
+          >
+            Show All Trips
+          </Button>
+        </div>
+      </div>
+
+      <p>You can view your trip details once this feature is ready...</p>
+    </Page>
+  );
 }
 
 export default TripDetails;
