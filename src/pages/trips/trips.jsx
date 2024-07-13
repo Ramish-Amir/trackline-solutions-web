d2807919228878af97a1e76ea1c4601523d8890f
amit097c <amit097@yahoo.com>
2024-07-13 17:25:09 -0400

    date fix

--- src/pages/trips/trips.jsx
+++ src/pages/trips/trips.jsx
@@ -83,9 +83,9 @@ const Trips = () => {
                 <TableCell>{(trip.ownerName)}</TableCell>
                 <TableCell>{trip.duration}</TableCell>
                 <TableCell>{trip.startingAddress}</TableCell>
-                <TableCell>{new Date(trip.startingTime *1000).toLocaleString()}</TableCell>
+                <TableCell>{new Date(trip.startingTime.seconds*1000+trip.startingTime.seconds/1000000).toLocaleString()}</TableCell>
                 <TableCell>{trip.endingAddress}</TableCell>
-                <TableCell>{new Date(trip.endingTime*1000).toLocaleString()}</TableCell>
+                <TableCell>{new Date(trip.endingTime.seconds*1000+trip.endingTime.seconds/1000000).toLocaleString()}</TableCell>
                 <TableCell>{trip.company}</TableCell>
                 <TableCell>
                   <DeleteOutlineIcon
